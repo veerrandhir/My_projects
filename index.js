2f18dc81e614cc5f94c0b70069c29f2a6734023a
veerrandhir <veerrandhir3@gmail.com>
2024-12-13 14:38:53 +0000

    Coding new thing

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 
+const bodyE1 = document.querySelector('body');
+bodyE1
+addEventListener('mousemove', ()=>{
+    const xoffset = event.offsetX;
+    const yoffset = event.offsetY;
+    const spanE1 = document.createElement('span');
+    bodyE1.appendChild(spanE1)
+})
+console.log('My heart is full');
+
+
+
